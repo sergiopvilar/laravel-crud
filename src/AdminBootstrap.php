606e9f8c49ef8ded98e7e7775b62e3697c66d312
Sergio Vilar <sergiovilar.r@gmail.com>
2015-11-01 13:46:04 -0300

    Added Form provider and class aliases for Lumen

--- src/AdminBootstrap.php
+++ src/AdminBootstrap.php
@@ -8,7 +8,14 @@
  */
 class AdminBootstrap {
 
-  public function __construct($path) {
+  public function __construct($path, $app = false) {
+
+    if($app) {
+      $app->register('Collective\Html\HtmlServiceProvider');
+      class_alias('Collective\Html\FormFacade', 'Form');
+      class_alias('Collective\Html\HtmlFacade', 'Html');
+      class_alias('Laravel\Lumen\Routing\UrlGenerator', 'Illuminate\Routing\UrlGenerator');
+    }
 
     $folder = realpath(__DIR__.'/../../../../'.$path);
     foreach (glob($folder . "/*.php") as $file) {
